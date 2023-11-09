00851a8ed02c1e4fea47238ac3927f3619173097
samar.it <samar.it@SAMAR>
2023-11-09 12:06:32 +0200

    add search and export elements

--- Bookify.web/wwwroot/js/site.js
+++ Bookify.web/wwwroot/js/site.js
@@ -1,5 +1,10 @@
 ﻿
-var updatedRow;
+var updatedRow,
+ table,
+ datatable,
+exportedCloumns = [];
+
+//alerts
 function showSuccessMessage(message = 'Saved successfully') {
     Swal.fire({
         text: message,
@@ -23,21 +28,119 @@ function showErrorMessage(message = 'something went wrong') {
         }
     });
 }
-function onModelSuccess(item) {
+
+//model
+function onModelSuccess(row) {
 
     showSuccessMessage();
     window.$('#Model').modal('hide');
-    if (updatedRow === undefined) {
-        $('tbody').append(item);
-    }
-    else {
-        $(updatedRow).replaceWith(item);
+    if (updatedRow !== undefined) {
+        datatable.row(updatedRow).remove().draw();
         updatedRow = undefined;
-    }
+    }  
+    var newRow = $(row);
+    datatable.row.add(newRow).draw();
+
     KTMenu.init();
     KTMenu.initHandlers();
 }
 
+//datatables
+var KTDatatables = function () {
+    //exclude action cloum
+    var headres = $('th');
+    $.each(headres, function (i) {       
+        var col = $(this);
+        if (!col.hasClass('js-no-export'))             
+            exportedCloumns.push(i);  
+    })
+  
+    // Private functions
+    var initDatatable = function () {
+
+        // Init datatable --- more info on datatables: https://datatables.net/manual/
+        datatable = $(table).DataTable({
+            "info": false,
+            'order': [],
+            'pageLength': 10,
+        });
+    }
+
+    // Hook export buttons
+    var exportButtons = () => {
+        const documentTitle = $(".js-datatables").data("documentTitle");
+        var buttons = new $.fn.dataTable.Buttons(table, {
+            buttons: [
+                {
+                    extend: 'copyHtml5',
+                    title: documentTitle,
+                    exportOptions: {
+                        columns: exportedCloumns
+                    }
+                },
+                {
+                    extend: 'excelHtml5',
+                    title: documentTitle,
+                    exportOptions: {
+                        columns: exportedCloumns
+                    }
+                },
+                {
+                    extend: 'csvHtml5',
+                    title: documentTitle,
+                    exportOptions: {
+                        columns: exportedCloumns
+                    }
+                },
+                {
+                    extend: 'pdfHtml5',
+                    title: documentTitle,
+                    exportOptions: {
+                        columns: exportedCloumns
+                    }
+                }
+            ]
+        }).container().appendTo($('#kt_datatable_example_buttons'));
+
+        // Hook dropdown menu click event to datatable export buttons
+        const exportButtons = document.querySelectorAll('#kt_datatable_example_export_menu [data-kt-export]');
+        exportButtons.forEach(exportButton => {
+            exportButton.addEventListener('click', e => {
+                e.preventDefault();
+
+                // Get clicked export value
+                const exportValue = e.target.getAttribute('data-kt-export');
+                const target = document.querySelector('.dt-buttons .buttons-' + exportValue);
+
+                // Trigger click event on hidden datatable export buttons
+                target.click();
+            });
+        });
+    }
+
+    // Search Datatable --- official docs reference: https://datatables.net/reference/api/search()
+    var handleSearchDatatable = () => {
+        const filterSearch = document.querySelector('[data-kt-filter="search"]');
+        filterSearch.addEventListener('keyup', function (e) {
+            datatable.search(e.target.value).draw();
+        });
+    }
+
+    // Public methods
+    return {
+        init: function () {
+            table = document.querySelector('.js_datatables');
+
+            if (!table) {
+                return;
+            }
+
+            initDatatable();
+            exportButtons();
+            handleSearchDatatable();
+        }
+    };
+}();
 $(document).ready(function () {
 
     //show edit and add success message

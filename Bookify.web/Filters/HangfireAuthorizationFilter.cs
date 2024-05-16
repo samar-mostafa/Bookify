39828ebd78c5a62e84df5aaf43e6ea33abcb0743
samar.it <samar.it@SAMAR>
2024-05-16 13:34:52 +0200

    background jobs using hangfire

---
+++ Bookify.web/Filters/HangfireAuthorizationFilter.cs
@@ -0,0 +1,23 @@
+﻿using Hangfire.Annotations;
+using Hangfire.Dashboard;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Bookify.web.Filters
+{
+    public class HangfireAuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        private string _policyName;
+        public HangfireAuthorizationFilter(string policyName)
+        {
+            _policyName = policyName;
+        }
+        public bool Authorize([NotNull] DashboardContext context)
+        {
+            var httpContext=context.GetHttpContext();
+            var authService = httpContext.RequestServices.GetRequiredService<IAuthorizationService>();
+            var isAuthorized = authService.AuthorizeAsync(httpContext.User,_policyName)
+                .ConfigureAwait(true).GetAwaiter().GetResult().Succeeded;
+            return isAuthorized;
+        }
+    }
+}

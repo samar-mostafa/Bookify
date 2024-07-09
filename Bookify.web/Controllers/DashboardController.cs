e3ac10f12b5ed766367cf9a6a6a78ea85c098c9c
samar <samar@DESKTOP-NRDSOTV>
2024-07-09 23:04:32 +0300

    dashboard

--- Bookify.web/Controllers/DashboardController.cs
+++ Bookify.web/Controllers/DashboardController.cs
@@ -6,9 +6,59 @@ namespace Bookify.web.Controllers
     [Authorize]
     public class DashboardController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public DashboardController(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var noOfCopies = _context.BookCopies.Count(c=>!c.IsDeleted);
+            var noOfCSubscribers = _context.Subscripers.Count(c=>!c.IsDeleted);
+            var lastAddedBooks = _context.Books.Include(b=>b.Author).
+                Where(b=>!b.IsDeleted).
+                OrderByDescending(b => b.Id)
+                .Take(8).ToList();
+
+            var topBooks = _context.RentalCopies.
+                Include(c => c.BookCopy)
+                .ThenInclude(c => c!.Book)
+                .ThenInclude(b => b!.Author)
+                .GroupBy(c => new
+                {
+                    c.BookCopy!.BookId,
+                    c.BookCopy.Book!.Title,
+                    c.BookCopy.Book.ImageThumbnailUrl,
+                    AuthorName = c.BookCopy.Book.Author!.Name,
+                }).Select(c => new
+                {
+                    c.Key.BookId,
+                    c.Key.Title,
+                    c.Key.ImageThumbnailUrl,
+                    c.Key.AuthorName,
+                    Count = c.Count()
+                }).OrderByDescending(c => c.Count)
+                .Take(6)
+                .Select(c => new BookViewModel
+                {
+                    Id = c.BookId,
+                    Title = c.Title,
+                    ImageThumbnailUrl = c.ImageThumbnailUrl,
+                    Author = c.AuthorName
+                }).ToList();
+            var model = new DashboardViewModel
+            {
+                NumberOfCopies=noOfCopies,
+                NumberOfSubscribers=noOfCSubscribers,
+                LastAddedBooks=_mapper.Map<IEnumerable<BookViewModel>>(lastAddedBooks),
+                TopBooks=topBooks
+            };
+
+            return View(model);
         }
     }
 }
